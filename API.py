6b7871988b55ba0f61ac029fec04c4af5a318695
Miguel Kami <miguelkami96@gmail.com>
2023-05-10 18:05:38 -0700

    API AdressValidation

---
+++ API.py
@@ -0,0 +1,23 @@
+import requests
+
+import json
+
+tListAdress     = []
+
+tListPostalCode = ['01153000','20050000','70714020']
+
+for PostalCode in tListPostalCode:
+
+    tURL     = 'https://viacep.com.br/ws/{}/json/'.format(PostalCode)
+
+    tRequest = requests.get(tURL)
+
+    tAdress  = tRequest.json()
+
+    # print(tAdress) 
+
+    tListAdress.append([tAdress["cep"],tAdress["logradouro"],tAdress["complemento"],tAdress["uf"]])
+
+for ListAdress in tListAdress:
+
+    print(ListAdress)
